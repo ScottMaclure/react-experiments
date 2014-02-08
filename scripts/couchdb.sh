462b1f0dab8f576e1bf8f9f47ff7af44702245b5
Scott Maclure <scott@maclure.com.au>
2014-02-09 07:52:29 +1100

    Fix to couchdb to use bash

--- scripts/couchdb.sh
+++ scripts/couchdb.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # Please make sure that you use correct hosts
 curl -X PUT http://127.0.0.1:5984/users
 curl -X PUT http://127.0.0.1:5984/users/_design/info/ -d '{"language":"javascript","views":{"full":{"map":"function(doc) {emit(doc.firstName, doc);}"}}}'
