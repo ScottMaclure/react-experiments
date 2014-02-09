ecf2869ebaaea06e9c8a8eec118c0b3eb963a6d9
Scott Maclure <scott@maclure.com.au>
2014-02-09 16:22:27 +1100

    Sync public folder into gh-pages repo.

--- app/marionette/PaginatedTableView.js
+++ app/marionette/PaginatedTableView.js
@@ -12,20 +12,28 @@ var PaginatedTableView = Backbone.Marionette.ItemView.extend({
 
 		this.rowTemplateHtml = options.rowTemplateHtml || '';
 		this.paginationTemplateHtml = options.paginationTemplateHtml || '';
+		this.tableActionsHtml = options.tableActionsHtml || '';
 
 		// Init pagination with default values, if not supplied by caller.
 		this.initPagination(options.pagination || {});
 
+		// Used for slowing down the search code until the user pauses.
+		this.delayedSearchTimer = null;
+
 	},
 
 	ui: {
+		tableActionsContainer: '.tableActionsContainer',
 		rowsContainer: '.rowsContainer',
-		paginationContainer: '.paginationContainer'
+		paginationContainer: '.paginationContainer',
+		searchRow: '.searchRow'
 	},
 
 	events: {
 		'click .pagination > li > a': 'doPagination',
-		'change .perPageSelector': 'doPerPageChange'
+		'change .perPageSelector': 'doPerPageChange',
+		'click .toggleSearch': 'toggleSearch',
+		'keyup .searchRow input': 'doDelayedSearch'
 	},
 
 	/**
@@ -37,9 +45,7 @@ var PaginatedTableView = Backbone.Marionette.ItemView.extend({
 			perPage: 50,
 			currentPage: 0,
 			perPageSelectorOptions: [25, 50, 100, 500]
-		}, (this.pagination || {}), options);
-
-		this.pagination.totalPages = Math.ceil(this.collection.length / this.pagination.perPage);
+		}, (this.pagination || {}), options || {});
 
 		// Group models into pages.
 		this.pageModels();
@@ -53,10 +59,33 @@ var PaginatedTableView = Backbone.Marionette.ItemView.extend({
 	 * Use groupBy to split models into arrays of models, using perPage as the grouping number.
 	 * Only needs to be called ONCE.
 	 */
-	pageModels: function () {
-		
-		this.pagedModels = this.collection.groupBy(function (model, index) {
+	pageModels: function (options) {
+
+		options = options || {};
+
+		var models;
+
+		if (options.filter) {
+
+			models = this.collection.filter(function (model) {
+				return -1 !== model.get(options.filter.key).toLowerCase().indexOf(options.filter.value);
+			});
+
+		} else {
+
+			models = this.collection.models;
+
+		}
+
+		// And recalc total.
+		this.pagination.totalModels = models.length;
+		this.pagination.totalPages = Math.ceil(models.length / this.pagination.perPage);
+
+		// Group models into a set of arrays of models, for pagination to use.
+		this.pagedModels = _.groupBy(models, function (model, index) {
+
 			return Math.floor(index / this.pagination.perPage);
+
 		}.bind(this));
 
 	},
@@ -71,7 +100,7 @@ var PaginatedTableView = Backbone.Marionette.ItemView.extend({
 
 		this.pagination.endPagerRange = this.pagination.currentPage + 5;
 		this.pagination.endPagerRange = this.pagination.endPagerRange >= this.pagination.totalPages ? 
-			this.pagination.totalPages : this.pagination.endPagerRange
+			this.pagination.totalPages : this.pagination.endPagerRange;
 
 	},
 
@@ -120,6 +149,10 @@ var PaginatedTableView = Backbone.Marionette.ItemView.extend({
 
 		console.time('PaginatedTableView#onRender');
 
+		this.ui.tableActionsContainer.html(_.template(
+			this.tableActionsHtml, { pagination: this.pagination }, { variable: 'data' }
+		));
+
 		// Render new rows in one go, rather than one-at-a-time.
 		var out = [];
 		_.each(this.pagedModels[this.pagination.currentPage], function (model) {
@@ -137,6 +170,51 @@ var PaginatedTableView = Backbone.Marionette.ItemView.extend({
 
 		console.timeEnd('PaginatedTableView#onRender');
 
+	},
+
+	toggleSearch: function () {
+		console.debug('toggleSearch called');
+		this.ui.searchRow.toggleClass('hide');
+	},
+
+	/**
+	 * Use a timeout to only kick off the search after X millis.
+	 * What about using throttle? Nah.
+	 */
+	doDelayedSearch: function (e) {
+		clearTimeout(this.delayedSearchTimer);
+		var $elem = $(e.target);
+		this.delayedSearchTimer = setTimeout(function () {
+			this.doSearch($elem.data('fieldName'), $elem.val());
+		}.bind(this), 300);
+	},
+
+	doSearch: function (fieldName, fieldValue) {
+
+		console.debug('doSearch, fieldName:', fieldName, 'fieldValue:', fieldValue);
+
+		this.pagination.currentPage = 0;
+
+		// Reconfigure pagination, after filtering collection.
+		this.pageModels({
+			filter: {
+				key: fieldName,
+				value: fieldValue
+			}
+		});
+
+		// Get the pagination boundaries sorted.
+		this.updatePagerRange();
+
+		// And re-render.
+		this.onRender();
+
+	},
+
+	serializeData: function () {
+		return {
+			pagination: this.pagination
+		};
 	}
 
 });
